0d64bc5c17c46f4ef86fabad81642204d90281d5
Sultan Kadyrkesh <sultankadyr@outlook.com>
2018-06-07 18:24:44 +0600

    AppContext, Web

---
+++ ASU.DTO/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASU.DTO
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
